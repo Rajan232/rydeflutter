a322c6559e9ad419016e2e90853938e3a538ae87
Rajan232 <rajan.pusalkar@gmail.com>
2024-05-08 11:07:39 +1000

    added colortheme in colors.dart file

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,3 +1,14 @@
 import 'package:flutter/material.dart';
 
 //Defining the colours used in the app
+
+@immutable
+class AppColors{
+  final darkGrey = const Color(0xFF222222);
+  final primaryGreen = const Color(0xFF75FFCA);
+  final mutedGreen = const Color(0xFFB1FBDF);
+  final accentGreen = const Color(0xFFD1FFED);
+  final lightGrey = const Color(0xFFEDEDED);
+
+  const AppColors();
+}
